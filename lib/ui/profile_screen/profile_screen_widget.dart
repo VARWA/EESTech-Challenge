ae69147539430ea8ef24869c8a911757fbd00931
Богдан Лупанов <60575285+VARWA@users.noreply.github.com>
2023-09-26 12:33:14 +0300

    profile background update

--- lib/ui/profile_screen/profile_screen_widget.dart
+++ lib/ui/profile_screen/profile_screen_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:eestech_challenge_app/ui/profile_screen/widgets/avatar_widget.dart';
 import 'package:eestech_challenge_app/ui/profile_screen/widgets/full_name_widget.dart';
 import 'package:eestech_challenge_app/ui/profile_screen/widgets/radar_chart_widget.dart';
@@ -9,17 +11,69 @@ class ProfileScreenWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: const [
-        SizedBox(height: 30),
-        AvatarWidget(),
-        SizedBox(height: 17),
-        Align(
-          alignment: AlignmentDirectional.center,
-          child: FullNameWidget(),
+      children: [
+        SizedBox(
+          height: 315,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              ImageFiltered(
+                imageFilter: ImageFilter.blur(sigmaY: 80, sigmaX: 80),
+                //SigmaX and Y are just for X and Y directions
+                child: Image.network(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcXCw2f8wwb5lEBH_87kGDTu7RYYQqIr1_ranwQPafvU6a_AdzLlPH45P1nLIAhPJIlWM&usqp=CAU',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Column(
+                children: [
+                  SizedBox(height: 30),
+                  AvatarWidget(),
+                  SizedBox(height: 17),
+                  Align(
+                    alignment: AlignmentDirectional.center,
+                    child: FullNameWidget(),
+                  ),
+                  SizedBox(height: 30),
+                ],
+              ),
+            ],
+          ),
         ),
-        SizedBox(height: 30),
-        Divider(),
-        RadarChartWidget(),
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+            borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(10), topLeft: Radius.circular(10)),
+          ),
+          child: ClipRect(
+            child: Column(
+              children: [
+                Container(
+                  color: Theme.of(context).cardTheme.surfaceTintColor,
+                  child: const Row(
+                    children: [
+                      SizedBox(
+                        width: 40,
+                      ),
+                      Text(
+                        '938',
+                        style: TextStyle(fontSize: 70, color: Colors.white),
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                        size: 60,
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(),
+                RadarChartWidget(),
+              ],
+            ),
+          ),
+        )
       ],
     );
   }
